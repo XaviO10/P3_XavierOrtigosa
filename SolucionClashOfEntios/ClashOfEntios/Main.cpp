1813690f7887a5d67a6bc6b74dc2feec4e4f0c16
XaviO10 <xaviercortes098@gmail.com>
2017-05-25 13:55:37 +0200

    adv

--- SolucionClashOfEntios/ClashOfEntios/Main.cpp
+++ SolucionClashOfEntios/ClashOfEntios/Main.cpp
@@ -4,18 +4,24 @@
 #include "Renderer.hh"
 int main() 
 {
-	while (true)
-	{
+	
 		enti::cout << "¿Que quieres hacer?" << enti::endl;
 		enti::cout << enti::cend;
+
 		enti::InputKey key;	
-		key = enti::getInputKey();
+		
+
 		Master map;
 		Player player(map);
 		map.inimap();
-		map.printmap();
-		player.input(key);
-	}
+		/*map.printmap();*/
+		while (true)
+		{
+			key = enti::getInputKey();				
+			player.input(key);
+		}
+		
+	
 	
 
 	return 0;	
